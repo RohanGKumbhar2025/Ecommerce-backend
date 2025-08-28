bb63cdd88eaa080fb83ca1049190199e8e56b4f7
rohan kumbhar <rohankumbhar993@gmail.com>
2025-08-29 00:47:06 +0530

    changes project 2

--- src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
+++ src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
@@ -2,10 +2,12 @@ package com.ecom.productcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan; // 1. Import this
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.ecom.productcatalog.model") // 2. Add this line
+@EntityScan("com.ecom.productcatalog.model")
+@EnableJpaRepositories("com.ecom.productcatalog.repository")
 public class ProductcatalogApplication {
 
 	public static void main(String[] args) {
