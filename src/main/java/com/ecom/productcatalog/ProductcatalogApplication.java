8b926728cf434da4ae957c778344d6997cc467f1
rohan kumbhar <rohankumbhar993@gmail.com>
2025-08-29 00:38:57 +0530

    changes project 2

--- src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
+++ src/main/java/com/ecom/productcatalog/ProductcatalogApplication.java
@@ -2,8 +2,10 @@ package com.ecom.productcatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan; // 1. Import this
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.ecom.productcatalog.model") // 2. Add this line
 public class ProductcatalogApplication {
 
 	public static void main(String[] args) {
